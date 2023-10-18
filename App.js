5f54b1867ab9ebfef127e5dc6a61fb26e4a0eb96
DeelynKsks <zaikeny77@gmail.com>
2023-10-18 16:28:59 -0300

    Formulario de carga de datos - Faltan validaciones; Solo muestra los datos por consola aún

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
-import { Button,Text ,TextInput, View, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Button, Text ,TextInput, View, StyleSheet,ScrollView, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [rows, setRows] = useState([{ parada: 1, presion: '', temperatura: '', profundidad: '', densidad: '' }]);
+  const scrollViewRef = useRef();
 
   const addRow = () => {
     const newRow = { parada: rows.length + 1, presion: '', temperatura: '', profundidad: '', densidad: '' };
     setRows([...rows, newRow]);
+    scrollViewRef.current.scrollToEnd({ animated: true });
   };
 
   const deleteRow = () => {
@@ -21,8 +23,13 @@ export default function App() {
     setRows(newRows);
   };
 
+  const sendData = () => {
+    console.log(JSON.stringify(rows));
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container} ref={scrollViewRef}>
+      <View style={{marginBottom: 30}}></View>
       {rows.map((row, index) => (
         <View key={index} style={styles.row}>
           <Text>Parada: {row.parada}</Text>
@@ -32,9 +39,23 @@ export default function App() {
           <TextInput placeholder="Densidad" onChangeText={(value) => updateRow(index, 'densidad', value)} />
         </View>
       ))}
-      <Button title="Agregar fila" onPress={addRow} />
-      <Button title="Borrar fila" onPress={deleteRow} />
-    </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity style={styles.button} onPress={addRow}>
+            <Text style={{textAlign: 'center', fontSize: 17}}>Agregar fila</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity style={styles.button} onPress={deleteRow}>
+            <Text style={{textAlign: 'center', fontSize: 17}}>Borrar fila</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.sendButton} onPress={sendData}>
+        <Text style={{textAlign: 'center', fontSize: 17}}>Enviar</Text>
+      </TouchableOpacity>
+      <View style={{marginBottom: 30}}></View>
+    </ScrollView>
   );
 }
 
@@ -48,4 +69,31 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 10,
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginBottom: 20, // o cualquier valor que desees para el espacio entre los botones y el siguiente elemento
+    justifyContent: "space-between",
+  },
+  buttonWrapper: {
+    width: '45%', // ajusta este valor para cambiar el espacio entre los botones
+  },
+  button: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "#000000",
+    backgroundColor: "#efd510",
+    width: '100%',
+    justifyContent: "center"
+  },
+  sendButton:{
+    borderStyle:'solid',
+    borderWidth :1,
+    borderRadius :5,
+    borderColor :"#000000",
+    backgroundColor :"#efd510",
+    width :'50%',
+    alignSelf: 'center',
+    marginTop :10
+  }
 });
